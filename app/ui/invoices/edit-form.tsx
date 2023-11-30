e32706db801cdf2f01ef984bc080fb204882c945
Aandi1989 <navaliny1989@gmail.com>
2023-11-30 11:32:30 +0300

    handling errors & improving accessibility

--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -10,6 +10,7 @@ import {
 } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import { Button } from '@/app/ui/button';
+import { useFormState } from 'react-dom';
 
 export default function EditInvoiceForm({
   invoice,
@@ -18,10 +19,14 @@ export default function EditInvoiceForm({
   invoice: InvoiceForm;
   customers: CustomerField[];
 }) {
+  const initialState = { message: null, errors: {} };
   const updateInvoiceWithId = updateInvoice.bind(null, invoice.id);
+  const [state, dispatch] = useFormState(updateInvoiceWithId, initialState);
+
+  console.log(state)
 
   return (
-    <form action={updateInvoiceWithId}>
+    <form action={dispatch}>
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
         {/* Customer Name */}
         <div className="mb-4">
@@ -66,6 +71,7 @@ export default function EditInvoiceForm({
               />
               <CurrencyDollarIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
             </div>
+            {state.errors?.amount && <p className="mt-2 text-sm text-red-500">{state.errors.amount}</p>}
           </div>
         </div>
 
@@ -110,6 +116,7 @@ export default function EditInvoiceForm({
               </div>
             </div>
           </div>
+          {state.errors && <p className="mt-2 text-sm text-red-500">{state.message}</p>}
         </fieldset>
       </div>
       <div className="mt-6 flex justify-end gap-4">
